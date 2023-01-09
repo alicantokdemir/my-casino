4f50cfcaf140315762d8e7322af08e340dee466b
Ali <alicantokdemir@gmail.com>
2023-01-08 23:09:33 -0300

    angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { games } from './games';
+
+function getGridColCount(innerWidth: number) {
+  if (innerWidth <= 450) {
+    return 1;
+  } else if (innerWidth <= 600) {
+    return 2;
+  } else if (innerWidth <= 800) {
+    return 3;
+  } else if (innerWidth <= 1000) {
+    return 4;
+  } else {
+    return 5;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  breakpoint = 5;
+  ngOnInit() {
+    this.breakpoint = getGridColCount(window.innerWidth);
+  }
+
+  onResize(event: any) {
+    this.breakpoint = getGridColCount(event.target?.innerWidth);
+  }
+
   title = 'my-casino';
+
+  games = games;
+
+  gameTabs = [
+    {
+      label: 'Top Games',
+    },
+    {
+      label: 'New Games',
+    },
+    {
+      label: 'Slots',
+    },
+    {
+      label: 'Jackpots',
+    },
+    {
+      label: 'Live',
+    },
+    {
+      label: 'Blackjack',
+    },
+    {
+      label: 'Roulette',
+    },
+    {
+      label: 'Table',
+    },
+    {
+      label: 'Poker',
+    },
+    {
+      label: 'Other',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
