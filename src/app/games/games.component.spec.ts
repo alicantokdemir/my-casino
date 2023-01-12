6a57accb38dc86ea207fef0a198312274876a57d
Ali <alicantokdemir@gmail.com>
2023-01-12 17:48:43 -0300

    some e2e tests

--- src/app/games/games.component.spec.ts
+++ src/app/games/games.component.spec.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed, waitForAsync, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { GamesListComponent } from './games/games.component';
-import { GamesService } from './games/games.service';
-import { JackpotsService } from './jackpots/jackpots.service';
+import { GamesListComponent } from './games.component';
+import { GamesService } from './games.service';
+import { JackpotsService } from '../jackpots/jackpots.service';
 import { StoreModule } from '@ngrx/store';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTabsModule } from '@angular/material/tabs';
-import { gamesReducer } from './state/games.reducer';
-import { jackpotsReducer } from './state/jackpots.reducer';
-import { Jackpot } from './jackpots/jackpots.model';
-import { Game } from './games/games.model';
+import { gamesReducer } from '../state/games.reducer';
+import { jackpotsReducer } from '../state/jackpots.reducer';
+import { Jackpot } from '../jackpots/jackpots.model';
+import { Game } from './games.model';
 import { of } from 'rxjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -52,13 +51,13 @@ const MOCK_GAMES: Game[] = [
   },
 ];
 
-describe('AppComponent', () => {
+describe('GamesListComponent', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let gamesService: GamesService;
   let jackpotsService: JackpotsService;
 
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+  let component: GamesListComponent;
+  let fixture: ComponentFixture<GamesListComponent>;
 
   beforeEach(waitForAsync(() => {
     const gamesServiceSpy = jasmine.createSpyObj('GamesService', {
@@ -83,21 +82,51 @@ describe('AppComponent', () => {
           {}
         ),
       ],
-      declarations: [AppComponent, GamesListComponent],
+      declarations: [GamesListComponent, GamesListComponent],
       providers: [
         { provide: GamesService, useValue: gamesServiceSpy },
         { provide: JackpotsService, useValue: jackpotsServiceSpy },
       ],
     }).compileComponents();
+
+    gamesService = TestBed.inject(GamesService) as jasmine.SpyObj<GamesService>;
+    jackpotsService = TestBed.inject(
+      JackpotsService
+    ) as jasmine.SpyObj<JackpotsService>;
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(GamesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render all top games', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('.game-card').length).toBe(
+      MOCK_GAMES.length
+    );
+  });
+
+  it('should render all 10 tabs', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('.mat-mdc-tab').length).toBe(10);
+  });
+
+  it('should render all new games on tab change', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const newTab: HTMLElement = compiled.querySelectorAll(
+      '[role="tab"]'
+    )[1] as HTMLElement;
+    newTab.dispatchEvent(new MouseEvent('click'));
+    component.onTabChange({ index: 1 });
+    fixture.detectChanges();
+
+    expect(compiled.querySelectorAll('.game-card').length).toBe(2);
   });
 });
